ace28b336ae6e5d9fecb62c070a50a6d72ac0fa8
Panagiotis Vlantis <panagiotis.vlantis.mc@gmail.com>
2017-04-30 14:40:08 +0300

    Ensure (once again) that the sr-speedbar-window is dedicated

--- sr-speedbar.el
+++ sr-speedbar.el
@@ -440,17 +440,17 @@ of a speedbar-window.  It will be created if necessary."
           ;; Auto refresh speedbar content
           ;; if option `sr-speedbar-auto-refresh' is non-nil
           (sr-speedbar-handle-auto-refresh sr-speedbar-auto-refresh))
-	(set-window-buffer sr-speedbar-new-window (get-buffer sr-speedbar-buffer-name))
-	;; Dedicate `sr-speedbar-window' to speedbar-buffer.
-	;; 
-	;; In the end, do not make this window dedicated as
-	;; Emacs' window splitting mechanism behaves peculiarly
-	;; when dedicated windows are present.
-	;; 
-        ;;(set-window-dedicated-p sr-speedbar-new-window t)
-	;; Switch to initially selected window.
+        (set-window-buffer sr-speedbar-new-window (get-buffer sr-speedbar-buffer-name))
+        ;; Dedicate `sr-speedbar-window' to speedbar-buffer.
+        ;;
+        ;; In the end, do not make this window dedicated as
+        ;; Emacs' window splitting mechanism behaves peculiarly
+        ;; when dedicated windows are present.
+        ;;
+        (set-window-dedicated-p sr-speedbar-new-window t)
 	;; Fix the size of every window displaying the speedbar buffer.
 	(setq-local window-size-fixed 'width)
+        ;; Switch to initially selected window.
         (select-window current-window))
     (message "`sr-speedbar' has been created.")))
 
